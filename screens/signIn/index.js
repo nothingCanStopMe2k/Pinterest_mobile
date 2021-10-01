2756814bc761d3e4e2d8491a10c42c16268ea7a0
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-10-01 12:45:24 +0700

    setup

---
+++ screens/signIn/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const signIn = () => {
+  return (
+    <View>
+      <Text>this is a signIn page</Text>
+    </View>
+  );
+};
+
+export default signIn;
+
+const styles = StyleSheet.create({});
---
+++ screens/signUp/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const signUp = () => {
+  return (
+    <View>
+      <Text>This is a signUp page</Text>
+    </View>
+  );
+};
+
+export default signUp;
+
+const styles = StyleSheet.create({});
