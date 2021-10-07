df1069548008e8e042ef38ca165a26124a14b113
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-10-07 15:54:06 +0700

    Giao diện trang signin

--- screens/signIn/index.js
+++ screens/signIn/index.js
@@ -1,24 +1,240 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Dimensions,
+  FlatList,
+  Animated,
+  Image,
+  // Animated,
+  ImageBackground,
+} from "react-native";
 import { authService } from "../../services/auth.service";
+import features from "../../constants/features";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
-const signIn = () => {
-  authService
-    .register({
-      email: "123456@gmail.com",
-      password: "1234",
-      confirmPassword: "1234",
-    })
-    .then((res) => console.log("RES: ", res))
-    .catch((err) => console.log("ERR: ", err.message));
+const { width, height } = Dimensions.get("window");
+
+const signIn = ({ navigation }) => {
+  const newFeatureScrollX = useRef(new Animated.Value(0)).current;
+
+  // authService
+  //   .register({
+  //     email: "123456@gmail.com",
+  //     password: "1234",
+  //     confirmPassword: "1234",
+  //   })
+  //   .then((res) => console.log("RES: ", res))
+  //   .catch((err) => console.log("ERR: ", err.message));
+  const renderFeatures = () => {
+    return (
+      <Animated.FlatList
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        snapToInterval={width}
+        data={features}
+        decelerationRate={0}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 12,
+        }}
+        style={{
+          flexGrow: 3,
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: newFeatureScrollX } } }],
+          { useNativeDriver: false }
+        )}
+        renderItem={({ item, index }) => {
+          return (
+            <ImageBackground
+              style={{
+                width: width,
+                justifyContent: "flex-end",
+                alignItems: "center",
+              }}
+              source={item.bg}
+            >
+              <View
+                style={{
+                  width: width,
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={item.image}
+                  resizeMode="cover"
+                  style={{
+                    width: 140,
+                    height: 140,
+                    borderRadius: 100,
+                    marginBottom: 30,
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 20,
+                    textAlign: "center",
+                    width: "80%",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {item.title1}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    textAlign: "center",
+                    width: "100%",
+                    fontWeight: "bold",
+                    marginBottom: 15,
+                  }}
+                >
+                  {item.title2}
+                </Text>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    width: "80%",
+                    fontSize: 15,
+                  }}
+                >
+                  {item.subTitle}
+                </Text>
+              </View>
+            </ImageBackground>
+          );
+        }}
+      />
+    );
+  };
+
+  const renderDots = () => {
+    const dotPosition = Animated.divide(newFeatureScrollX, width);
+
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          marginVertical: 48,
+        }}
+      >
+        {features.map((item, index) => {
+          const opacity = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [0.3, 1, 0.3],
+            extrapolate: "clamp",
+          });
+          const dotWidth = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [6, 20, 6],
+            extrapolate: "clamp",
+          });
+          const dotColor = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: ["#444", "#FF002E", "#444"],
+            extrapolate: "clamp",
+          });
+
+          return (
+            <Animated.View
+              key={`dot-${index}`}
+              opacity={opacity}
+              style={{
+                borderRadius: 12,
+                marginHorizontal: 3,
+                width: dotWidth,
+                height: 6,
+                backgroundColor: dotColor,
+              }}
+            />
+          );
+        })}
+      </View>
+    );
+  };
+
+  const renderButtons = () => {
+    return (
+      <View
+        style={{
+          flexGrow: 1,
+          justifyContent: "flex-end",
+        }}
+      >
+        <TouchableWithoutFeedback
+          style={styles.buttonEmail}
+          onPress={() => {
+            navigation.navigate("signUp");
+          }}
+        >
+          <Text style={styles.text}>Đăng kí với Email</Text>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback style={styles.buttonFacebook}>
+          <Text style={styles.text}>Đăng nhập với Facebook</Text>
+        </TouchableWithoutFeedback>
+        <Text
+          style={{
+            textAlign: "center",
+            width: "100%",
+            textDecorationLine: "underline",
+            marginVertical: 30,
+          }}
+        >
+          Tìm hiểu chính sách của chúng tôi
+        </Text>
+      </View>
+    );
+  };
 
   return (
-    <View>
-      <Text>this is a signIn page</Text>
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.layout}>
+        {renderFeatures()}
+        {renderDots()}
+        {renderButtons()}
+      </View>
+    </>
   );
 };
 
 export default signIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  layout: {
+    width: width,
+    height: height,
+    flex: 1,
+  },
+  buttonEmail: {
+    backgroundColor: "#FF002E",
+    width: "85%",
+    marginHorizontal: "7.5%",
+    marginVertical: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 30,
+  },
+  buttonFacebook: {
+    backgroundColor: "blue",
+    width: "85%",
+    marginHorizontal: "7.5%",
+    marginVertical: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 30,
+  },
+  text: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
