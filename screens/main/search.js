f056833e8d275cc39961a343e9cb4a9e5e35b755
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-11-30 23:14:21 +0700

    Gan the tat ca cac anh trong database

--- screens/main/search.js
+++ screens/main/search.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+import { fileService } from "../../services/file.service";
 
 const Search = () => {
+  const [allFile, setAllFile] = useState([]);
+
+  useEffect(() => {
+    fileService.getAllFile().then((res) => {
+      setAllFile(res);
+    });
+  }, []);
+
+  const show = () => {
+    let count = 0;
+    for (var i of allFile) {
+      if (i.tag.length > 0) {
+        count++;
+      }
+    }
+    console.log(
+      "Tong cong " + count + "/" + allFile.length + " files duoc gan the"
+    );
+  };
+
   return (
     <View>
-      <Text>This is search Screens</Text>
+      <TouchableOpacity onPress={() => show()}>
+        <Text>Show tat ca anh duoc gan tag trong database</Text>
+      </TouchableOpacity>
     </View>
   );
 };
